8050e0bf386ba39da20f5da5f7233e2d93540058
abdulalimsujon <alimsujon12@gmail.com>
2022-07-21 03:17:20 +0600

    nested route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Friends from './component/Friends/Friends';
 import Header from './component/Header/Header';
 import Home from './component/Home/Home';
 import NotFound from './component/NotFound/NotFound';
+import PostDetail from './component/PostDetail/PostDetail';
+import Posts from './component/Posts/Posts';
 import DetailFriend from './DetailFriend/DetailFriend';
 
 function App() {
@@ -18,6 +20,13 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/Friends' element={<Friends></Friends>}></Route>
         <Route path='/buddy/:buddyId' element={<DetailFriend></DetailFriend>}></Route>
+        <Route path='/posts' element={<Posts></Posts>}>
+          <Route path=":postId" element={<PostDetail></PostDetail>}>
+
+          </Route>
+
+
+        </Route>
         <Route path='/About' element={<About></About>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
